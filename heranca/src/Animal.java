0ddac74e26de5d8c91ee4376665c0f883bb6b36b
JOAO PEDRO SILVA TEIXEIRA <joao_ps_teixeira@estudante.sesisenai.org.br>
2024-05-09 16:56:55 -0300

    getters and setters

--- heranca/src/Animal.java
+++ heranca/src/Animal.java
@@ -22,6 +22,52 @@ public class Animal {
 
     }
 
-    
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNomeCientifico() {
+        return nomeCientifico;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
 }
